e0666cb787a3bee0c5edd08428b6519367add27a
plbin97 <i@teenet.me>
2022-02-10 20:03:07 -0500

    update example

--- src/Robot.py
+++ src/Robot.py
@@ -3,6 +3,7 @@ from src.handleTestRequest import handleTestRequest
 class Robot(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
+        print('You can have your test now')
 
     async def on_message(self, message):
         await handleTestRequest(self, message)

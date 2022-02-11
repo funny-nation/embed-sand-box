276d7eb7ba5258d67532c317daeff4f460a50b65
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-11 03:43:45 -0500

    Update openLottery.py

--- embed/openLottery.py
+++ embed/openLottery.py
@@ -3,8 +3,8 @@ from discord import Embed
 
 def getEmbed() -> Embed:
     embed = Embed()
-    embed.title = "幸运抽奖" #大标题
-    embed.description = "恭喜！@中奖人1 \n中奖人2 \n……" #详情说明
+    embed.title = "开奖啦！" #大标题
+    embed.description = "恭喜！@中奖人1\n中奖人2\n……" #详情说明
     embed.add_field(name="获得由@发奖人提供的奖品{奖品名称}", value="记得及时兑奖哦！", inline=False) #小标题3
     embed.colour = 0xe34bab #0x留着，后面加颜色编号
     embed.set_thumbnail(url="https://www.funnynation.org/wp-content/uploads/2022/02/宇宙素材-3.png") #右上角的图

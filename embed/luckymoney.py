29cc2dc214dbbd3e480d0bba0fa2da5bca24a179
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-10 22:08:47 -0500

    Update luckymoney.py

--- embed/luckymoney.py
+++ embed/luckymoney.py
@@ -4,7 +4,8 @@ from discord import Embed
 def getEmbed() -> Embed:
     embed = Embed()
     embed.title = "抢红包啦！" #大标题
-    embed.description = "user_name的红包已经发出 ```\n点击反应抢红包啦！```" #详情说明
+    embed.description = "user_name的红包已经发出"
+    embed.add_field(name="点击反应抢红包啦！", inline=True)
     embed.colour = 0xff2121 #0x留着，后面加颜色编号
     embed.set_thumbnail(url="https://www.funnynation.org/wp-content/uploads/2022/02/红包.png") #右上角的图
 

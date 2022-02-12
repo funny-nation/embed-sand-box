1c97d32b7d041a086494431d966af1242e23ccf7
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-11 23:51:53 -0500

    Update list.py

--- embed/list.py
+++ embed/list.py
@@ -4,7 +4,7 @@ from discord import Embed
 def getEmbed() -> Embed:
     embed = Embed()
     embed.title = "GHS小花园富豪榜" #大标题
-    embed.description = "富豪榜列表" #详情说明
+    embed.description = "1: user - 100元 \n2：user - 99元 " #详情说明
     embed.colour = 0xfff18a #0x留着，后面加颜色编号
 
     return embed

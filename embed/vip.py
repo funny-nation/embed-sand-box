a6d5d50831e6a6571a5fa94113dffe08a36fe0e3
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-12 00:30:44 -0500

    Update vip.py

--- embed/vip.py
+++ embed/vip.py
@@ -4,7 +4,7 @@ from discord import Embed
 def getEmbed() -> Embed:
     embed = Embed()
     embed.title = "您的VIP升级啦！"
-    embed.description = "恭喜@user晋升为VIP10！记得常来玩儿哦！~"
+    embed.description = "恭喜@user晋升为尊贵的VIP10！记得常来玩儿哦！~"
     embed.colour = 0xbdf3ff
     embed.set_image(url="https://www.funnynation.org/wp-content/uploads/2022/02/圣火VIP.gif")
 

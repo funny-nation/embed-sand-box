612a0ed2526efbf50a714af4932c46d636e4e955
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-15 17:15:36 -0500

    Update wall.py

--- embed/wall.py
+++ embed/wall.py
@@ -3,7 +3,7 @@ from discord import Embed
 
 def getEmbed() -> Embed:
     embed = Embed()
-    embed.title = "匿名名称：匿名内容"  # 大标题里写
+    embed.description = "匿名名称：匿名信息"  # 详情说明
     embed.colour = 0x763cb5  # 颜色需要随机
 
     return embed

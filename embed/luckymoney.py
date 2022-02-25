b3a9306f886a3ac835a929f170c073d1737619e8
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-24 22:02:54 -0500

    Update luckymoney.py

--- embed/luckymoney.py
+++ embed/luckymoney.py
@@ -4,6 +4,7 @@ from discord import Embed
 def getEmbed() -> Embed:
     embed = Embed()
     embed.title = "user_name的红包已经发出!" #大标题
+    embed.description = "抢到最大的红包的就是小傻瓜"
     embed.add_field(name="-", value="恭喜user！你抢到了100元！", inline=False)
     embed.add_field(name="-", value="恭喜user！你抢到了100元！", inline=False)
     embed.add_field(name="-", value="恭喜user！你抢到了100元！", inline=False)

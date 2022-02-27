c5afc6319baf07c788ac8b8fb1bfde1ce80f61c0
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-27 11:10:25 -0500

    Update randomNumber.py

--- embed/randomNumber.py
+++ embed/randomNumber.py
@@ -3,7 +3,8 @@ from discord import Embed
 
 def getEmbed() -> Embed:
     embed = Embed()
-    embed.title = "随机数字" #大标题
+    embed.title = "0-100内的随机数字" #大标题
+    embed.description = "[1,2,3]"  # 详情说明
     embed.colour = 0x42ffd3 #0x留着，后面加颜色编号
 
     return embed

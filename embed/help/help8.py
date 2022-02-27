b201bc0b16d13ce929681f9fd0e1bd32cfbe50bf
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-02-27 10:08:25 -0500

    Update help8.py

--- embed/help/help8.py
+++ embed/help/help8.py
@@ -5,6 +5,6 @@ def getEmbed() -> Embed:
     embed = Embed()
     embed.title = "8 - GHS小花园匿名墙"
     embed.description = "私信bot指令：匿名 匿名内容" \
-                        "\nbot将发布你的匿名消息到GHS匿名墙" 
+                        "\nbot将发布你的匿名消息到GHS匿名墙"
     embed.colour = 0xff9900
     return embed

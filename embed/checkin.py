3a7a328ff876024e3b24167a29e8b55caaa06a4f
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-03-07 23:56:16 -0500

    Update checkin.py

--- embed/checkin.py
+++ embed/checkin.py
@@ -3,7 +3,7 @@ from discord import Embed
 
 def getEmbed() -> Embed:
     embed = Embed()
-    embed.title = "您已签到成功"
+    embed.title = "@user您已签到成功！"
     embed.description = "本次签到获得60货币！"
     embed.colour = 0xff9900
     return embed

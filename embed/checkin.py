d7988d7d4cd6b2d0dc024857e3d865d50ee3b538
RyoukaZZZ <97544306+RyoukaZ0417@users.noreply.github.com>
2022-03-07 23:34:11 -0500

    Create checkin.py

---
+++ embed/checkin.py
@@ -0,0 +1,9 @@
+from discord import Embed
+
+
+def getEmbed() -> Embed:
+    embed = Embed()
+    embed.title = "您已签到成功"
+    embed.description = "本次签到获得100货币！"
+    embed.colour = 0xff9900
+    return embed
